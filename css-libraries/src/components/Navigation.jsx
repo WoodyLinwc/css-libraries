0398bbf4d066740898e5ca5c53019243b85909ab
Woody Lin <wancheng.lin001@gmail.com>
2025-10-28 16:31:15 -0400

    Update Navigation.jsx

--- css-libraries/src/components/Navigation.jsx
+++ css-libraries/src/components/Navigation.jsx
@@ -4,6 +4,7 @@ import "../App.css";
 const Navigation = () => {
   return (
     <nav className="navigation">
+      <Link to="/">Home</Link>
       <Link to="/mui">Material UI</Link>
       <Link to="/antdesign">Ant Design</Link>
       <Link to="/chakraui">Chakra UI</Link>
